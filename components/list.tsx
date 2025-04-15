bfff70eb52cd40fcfddc723ea4ade1e5c1961d98
M40 <modev.404@gmail.com>
2025-04-15 17:41:31 +0300

    flat list

--- components/list.tsx
+++ components/list.tsx
@@ -1,33 +1,14 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { FlatList, Text } from "react-native";
 const List = () => {
   const data = Array.from({ length: 100 }).map(
     (_, index) => `Item ${index + 1}`
   );
   return (
-    <ScrollView>
-      <View style={style.container}>
-        {data.map((item, index) => (
-          <Text style={style.item} key={index}>
-            {item}
-          </Text>
-        ))}
-      </View>
-    </ScrollView>
+    <FlatList
+      data={data}
+      renderItem={({ item, index }) => <Text key={index}>{item}</Text>}
+    />
   );
 };
-
-const style = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 10,
-  },
-  item: {
-    backgroundColor: "skyblue",
-    marginTop: 10,
-    padding: 10,
-    borderRadius: 12,
-  },
-});
 
 export default List;
