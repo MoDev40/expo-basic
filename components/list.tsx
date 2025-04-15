d186631ef11a2f1fe298881f388e9c3a908ab5ec
M40 <modev.404@gmail.com>
2025-04-15 17:15:55 +0300

    scroll view

--- components/list.tsx
+++ components/list.tsx
@@ -1,16 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 const List = () => {
   const data = Array.from({ length: 100 }).map(
     (_, index) => `Item ${index + 1}`
   );
   return (
-    <View style={style.container}>
-      {data.map((item, index) => (
-        <Text style={style.item} key={index}>
-          {item}
-        </Text>
-      ))}
-    </View>
+    <ScrollView>
+      <View style={style.container}>
+        {data.map((item, index) => (
+          <Text style={style.item} key={index}>
+            {item}
+          </Text>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
