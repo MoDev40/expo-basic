b8b7025462c29f06feee6f6a31833fb89b415f53
M40 <modev.404@gmail.com>
2025-04-15 17:07:12 +0300

    styling

--- components/list.tsx
+++ components/list.tsx
@@ -1,15 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 const List = () => {
   const data = Array.from({ length: 100 }).map(
     (_, index) => `Item ${index + 1}`
   );
   return (
-    <View>
+    <View style={style.container}>
       {data.map((item, index) => (
-        <Text key={index}>{item}</Text>
+        <Text style={style.item} key={index}>
+          {item}
+        </Text>
       ))}
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+  },
+  item: {
+    backgroundColor: "skyblue",
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 12,
+  },
+});
 
 export default List;
