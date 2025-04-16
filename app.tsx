1c9b26cd08c3b7c184caa1e70810c731b51703da
M40 <modev.404@gmail.com>
2025-04-16 09:32:27 +0300

    updated dir

--- app.tsx
+++ app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text, View } from "react-native";
-import List from "./components/list";
-import Buttons from "./components/buttons";
-import Forms from "./components/form";
+import List from "./components/basic/list";
+import Buttons from "./components/basic/buttons";
+import Forms from "./components/basic/form";
 
 const App = () => {
   return (
