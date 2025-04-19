5617a357963e54775759459eea6620b8f19c4827
M40 <modev.404@gmail.com>
2025-04-19 14:01:34 +0300

    Stack layout screen navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,39 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Home", headerTintColor: "skyblue" }}
+      />
+      <Stack.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+      <Stack.Screen
+        name="users/index"
+        options={{
+          title: "Users",
+        }}
+      />
+      <Stack.Screen
+        name="users/[id]/index"
+        options={{
+          title: "User",
+        }}
+      />
+      <Stack.Screen
+        name="blogs/index"
+        options={{
+          title: "Blogs",
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default RootLayout;
